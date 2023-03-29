ff8566adf93c8260825847d83e587f711fe272c3
oskarth <oskarth@titanproxy.com>
2023-03-29 14:01:43 +0700

    Enable halo2 circuits and update README

---
+++ halo2/README.md
@@ -0,0 +1,5 @@
+# Halo2
+
+## How to run
+
+`cargo run --example sha256 --release 17 10`
--- halo2/examples/sha256.rs
+++ halo2/examples/sha256.rs
@@ -66,34 +66,35 @@ fn main() {
     let k: usize = args[2].parse().unwrap();
     let params_size: u32 = args[1].parse().unwrap();
 
-    //let params = ParamsKZG::<Bn256>::setup(params_size, OsRng);
+    let params = ParamsKZG::<Bn256>::setup(params_size, OsRng);
     let circuit = MyCircuit { iter_num: k };
 
-    use plotters::prelude::*;
-    let root = BitMapBackend::new("sha_layout.png", (1024, 7680)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-    let root = root
-        .titled(&format!("SHA - Depth={}", params_size), ("sans-serif", 60))
-        .unwrap();
+    // Plotting circuit
+    // use plotters::prelude::*;
+    // let root = BitMapBackend::new("sha_layout.png", (1024, 7680)).into_drawing_area();
+    // root.fill(&WHITE).unwrap();
+    // let root = root
+    //     .titled(&format!("SHA - Depth={}", params_size), ("sans-serif", 60))
+    //     .unwrap();
 
-    halo2_proofs::dev::CircuitLayout::default()
-        .render(params_size as u32, &circuit, &root)
-        .unwrap();
+    // halo2_proofs::dev::CircuitLayout::default()
+    //     .render(params_size as u32, &circuit, &root)
+    //     .unwrap();
 
-    // let vk = keygen_vk(&params, &circuit).unwrap();
-    // let pk = keygen_pk(&params, vk, &circuit).unwrap();
+    let vk = keygen_vk(&params, &circuit).unwrap();
+    let pk = keygen_pk(&params, vk, &circuit).unwrap();
 
-    // let start = start_timer!(|| "Compute Halo2 recursive hash");
-    // let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-    // create_proof::<KZGCommitmentScheme<_>, ProverGWC<_>, _, _, _, _>(
-    //     &params,
-    //     &pk,
-    //     &[circuit],
-    //     &[],
-    //     OsRng,
-    //     &mut transcript,
-    // )
-    // .expect("proof generation should not fail");
-    // let proof: Vec<u8> = transcript.finalize();
-    // end_timer!(start);
+    let start = start_timer!(|| "Compute Halo2 recursive hash");
+    let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+    create_proof::<KZGCommitmentScheme<_>, ProverGWC<_>, _, _, _, _>(
+        &params,
+        &pk,
+        &[circuit],
+        &[],
+        OsRng,
+        &mut transcript,
+    )
+    .expect("proof generation should not fail");
+    let proof: Vec<u8> = transcript.finalize();
+    end_timer!(start);
 }
