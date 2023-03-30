e22f6a6de02b79f5f0e29b2c9429fb41478d5c24
oskarth <oskarth@titanproxy.com>
2023-03-30 12:03:43 +0700

    Get back to runnable state

--- nova/examples/sha256_wasm.rs
+++ nova/examples/sha256_wasm.rs
@@ -6,7 +6,7 @@ use ff::PrimeField;
 use ff::derive::bitvec::vec;
 use nova_scotia::{
     circom::reader::load_r1cs, create_public_params, FileLocation, F1, F2,
-    G1, G2, S1, S2, create_recursive_circuit_alt, create_public_params_alt,
+    G1, G2, S1, S2, create_recursive_circuit,
 };
 // Ignore create_recursive_circuit
 
@@ -39,109 +39,110 @@ fn gen_nth_sha256_hash(n: usize) -> Vec<u8> {
     hash
 }
 
-// fn recursive_hashing(depth: usize) {
-//     println!{"Using recursive depth: {:?}", depth};
+fn recursive_hashing(depth: usize) {
+    println!{"Using recursive depth: {:?}", depth};
 
-//     let iteration_count = depth;
-//     let root = current_dir().unwrap();
+    let iteration_count = depth;
+    let root = current_dir().unwrap();
 
-//     let circuit_file = root.join("./examples/sha256/circom/sha256_test_nova.r1cs");
-//     let r1cs = load_r1cs(&FileLocation::PathBuf(circuit_file));
-//     let witness_generator_wasm = root.join("./examples/sha256/circom/sha256_test_nova_js/sha256_test_nova.wasm");
+    let circuit_file = root.join("./examples/sha256/circom/sha256_test_nova.r1cs");
+    let r1cs = load_r1cs(&FileLocation::PathBuf(circuit_file));
+    let witness_generator_wasm = root.join("./examples/sha256/circom/sha256_test_nova_js/sha256_test_nova.wasm");
 
-//     let mut in_vector = vec![];
-//     for i in 0..depth {
-//         in_vector.push(gen_nth_sha256_hash(i));
-//     }
+    let mut in_vector = vec![];
+    for i in 0..depth {
+        in_vector.push(gen_nth_sha256_hash(i));
+    }
  
-//     // println!("100th recursive SHA256 hash: {:?}", gen_nth_sha256_hash(100));
+    // println!("100th recursive SHA256 hash: {:?}", gen_nth_sha256_hash(100));
 
-//     let step_in_vector = vec![0; 32];
+    let step_in_vector = vec![0; 32];
     
-//    let mut private_inputs = Vec::new();
-//     for i in 0..iteration_count {
-//         let mut private_input = HashMap::new();
-//         private_input.insert("in".to_string(), json!(in_vector[i]));
-//         private_inputs.push(private_input);
-//     }
+   let mut private_inputs = Vec::new();
+    for i in 0..iteration_count {
+        let mut private_input = HashMap::new();
+        private_input.insert("in".to_string(), json!(in_vector[i]));
+        private_inputs.push(private_input);
+    }
 
-//     let start_public_input = step_in_vector.into_iter().map(|x| F1::from(x)).collect::<Vec<_>>();
+    let start_public_input = step_in_vector.into_iter().map(|x| F1::from(x)).collect::<Vec<_>>();
 
-//     let pp = create_public_params(r1cs.clone());
+    let pp = create_public_params(r1cs.clone());
 
-//     println!(
-//         "Number of constraints per step (primary circuit): {}",
-//         pp.num_constraints().0
-//     );
-//     println!(
-//         "Number of constraints per step (secondary circuit): {}",
-//         pp.num_constraints().1
-//     );
+    println!(
+        "Number of constraints per step (primary circuit): {}",
+        pp.num_constraints().0
+    );
+    println!(
+        "Number of constraints per step (secondary circuit): {}",
+        pp.num_constraints().1
+    );
 
-//     println!(
-//         "Number of variables per step (primary circuit): {}",
-//         pp.num_variables().0
-//     );
-//     println!(
-//         "Number of variables per step (secondary circuit): {}",
-//         pp.num_variables().1
-//     );
+    println!(
+        "Number of variables per step (primary circuit): {}",
+        pp.num_variables().0
+    );
+    println!(
+        "Number of variables per step (secondary circuit): {}",
+        pp.num_variables().1
+    );
 
-//     // create a recursive SNARK
-//     let timer_create_proof = start_timer!(|| "Create RecursiveSNARK");
-//     let recursive_snark = create_recursive_circuit(
-//         FileLocation::PathBuf(witness_generator_wasm),
-//         r1cs,
-//         private_inputs,
-//         start_public_input.clone(),
-//         &pp,
-//     )
-//     .unwrap();
-//     end_timer!(timer_create_proof);
+    // create a recursive SNARK
+    let timer_create_proof = start_timer!(|| "Create RecursiveSNARK");
+    let recursive_snark = create_recursive_circuit(
+        FileLocation::PathBuf(witness_generator_wasm),
+        r1cs,
+        private_inputs,
+        start_public_input.clone(),
+        &pp,
+    )
+    .unwrap();
+    end_timer!(timer_create_proof);
 
-//     // TODO: empty?
-//     let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
-
-
-//     // verify the recursive SNARK
-//     println!("Verifying a RecursiveSNARK...");
-//     let timer_verify_snark = start_timer!(|| "verify SNARK");
-//     let start = Instant::now();
-//     let res = recursive_snark.verify(
-//         &pp,
-//         iteration_count,
-//         start_public_input.clone(),
-//         z0_secondary.clone(),
-//     );
-//     assert!(res.is_ok());
-
-//     end_timer!(timer_verify_snark);
+    // TODO: empty?
+    let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
 
 
+    // verify the recursive SNARK
+    println!("Verifying a RecursiveSNARK...");
+    let timer_verify_snark = start_timer!(|| "verify SNARK");
+    let start = Instant::now();
+    let res = recursive_snark.verify(
+        &pp,
+        iteration_count,
+        start_public_input.clone(),
+        z0_secondary.clone(),
+    );
+    assert!(res.is_ok());
 
-//     // produce a compressed SNARK
-//     let timer_gen_compressed_snark = start_timer!(|| "Generate a CompressedSNARK using Spartan with IPA-PC");
-//     let start = Instant::now();
-//     let (pk, vk) = CompressedSNARK::<_, _, _, _, S1, S2>::setup(&pp).unwrap();
-//     let res = CompressedSNARK::<_, _, _, _, S1, S2>::prove(&pp, &pk, &recursive_snark);
-//     assert!(res.is_ok());
-//     let compressed_snark = res.unwrap();
-//     end_timer!(timer_gen_compressed_snark);
+    end_timer!(timer_verify_snark);
 
-//     let timer_verify_compressed_snark = start_timer!(|| "Verify CompressedSNARK");
-//     let start = Instant::now();
-//     let res = compressed_snark.verify(
-//         &vk,
-//         iteration_count,
-//         start_public_input.clone(),
-//         z0_secondary,
-//     );
-//     end_timer!(timer_verify_compressed_snark);
 
-//     assert!(res.is_ok());
-// }
+
+    // produce a compressed SNARK
+    let timer_gen_compressed_snark = start_timer!(|| "Generate a CompressedSNARK using Spartan with IPA-PC");
+    let start = Instant::now();
+    let (pk, vk) = CompressedSNARK::<_, _, _, _, S1, S2>::setup(&pp).unwrap();
+    let res = CompressedSNARK::<_, _, _, _, S1, S2>::prove(&pp, &pk, &recursive_snark);
+    assert!(res.is_ok());
+    let compressed_snark = res.unwrap();
+    end_timer!(timer_gen_compressed_snark);
+
+    let timer_verify_compressed_snark = start_timer!(|| "Verify CompressedSNARK");
+    let start = Instant::now();
+    let res = compressed_snark.verify(
+        &vk,
+        iteration_count,
+        start_public_input.clone(),
+        z0_secondary,
+    );
+    end_timer!(timer_verify_compressed_snark);
+
+    assert!(res.is_ok());
+}
 
 // Primary and secondary circuit optimized inputs for more efficient folding
+#[cfg(feature = "")]
 fn recursive_hashing2(depth: usize) {
 
     let iteration_count = depth;
@@ -277,6 +278,6 @@ fn main() {
     //let sha_block: u64 = args[2].parse().unwrap();
 
     // NOTE: Toggle here
-    //recursive_hashing(k);
-    recursive_hashing2(k);
+    recursive_hashing(k);
+    //recursive_hashing2(k);
 }
