fe41d7d1b4353eb75243d8864d6bdf9ed083c70f
oskarth <oskarth@titanproxy.com>
2023-04-04 16:56:24 +0700

    bitcoin parallel

--- nova/examples/bitcoin.rs
+++ nova/examples/bitcoin.rs
@@ -5,11 +5,21 @@ use std::{
     time::{Duration, Instant},
 };
 
+use ark_std::{end_timer, start_timer};
+
 use ff::PrimeField;
 use nova_scotia::{
-    circom::reader::load_r1cs, create_public_params, create_recursive_circuit, FileLocation, F1, G2,
+    circom::{circuit::CircomCircuit, reader::load_r1cs},
+    create_public_params, 
+    create_public_params_par,
+    create_recursive_circuit, FileLocation,
+    F1, F2, G2, G1, S1, S2,
+
 };
-use nova_snark::traits::Group;
+use nova_snark::{
+    parallel_prover::{FoldInput, NovaTreeNode, PublicParams},
+    traits::{circuit::TrivialTestCircuit, Group},
+};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
@@ -21,11 +31,13 @@ struct Blocks {
     blockHeaders: Vec<Vec<u8>>,
 }
 
-fn bench(iteration_count: usize, per_iteration_count: usize) -> (Duration, Duration) {
+fn bench_seq(iteration_count: usize, per_iteration_count: usize) -> (Duration, Duration) {
     let root = current_dir().unwrap();
 
+    let depth = iteration_count;
+
     let circuit_file = root.join("examples/bitcoin/circom/bitcoin_benchmark.r1cs");
-    let r1cs = load_r1cs(&FileLocation::PathBuf(circuit_file));
+    let r1cs_circom = load_r1cs(&FileLocation::PathBuf(circuit_file));
     let witness_generator_file =
         root.join("examples/bitcoin/circom/bitcoin_benchmark_cpp/bitcoin_benchmark");
 
@@ -61,7 +73,7 @@ fn bench(iteration_count: usize, per_iteration_count: usize) -> (Duration, Durat
 
     // println!("{:?} {:?}", start_public_input, private_inputs);
 
-    let pp = create_public_params(r1cs.clone());
+    let pp = create_public_params(r1cs_circom.clone());
 
     println!(
         "Number of constraints per step (primary circuit): {}",
@@ -81,11 +93,12 @@ fn bench(iteration_count: usize, per_iteration_count: usize) -> (Duration, Durat
         pp.num_variables().1
     );
 
+    // Sequential case
     println!("Creating a RecursiveSNARK...");
     let start = Instant::now();
     let recursive_snark = create_recursive_circuit(
         FileLocation::PathBuf(witness_generator_file),
-        r1cs,
+        r1cs_circom,
         private_inputs,
         start_public_input.clone(),
         &pp,
@@ -93,6 +106,36 @@ fn bench(iteration_count: usize, per_iteration_count: usize) -> (Duration, Durat
     .unwrap();
     let prover_time = start.elapsed();
     println!("RecursiveSNARK creation took {:?}", start.elapsed());
+    //--------------------------------
+
+    // Parallel case
+    // let folds: Vec<FoldInput<G1>> = nova_scotia::prepare_folds(
+    //     FileLocation::PathBuf(witness_generator_file),
+    //     r1cs_circom.clone(),
+    //     private_inputs,
+    //     depth,
+    //     // This is wrong and we should be passing all the PIs here.
+    //     start_public_input.clone(),
+    // );
+
+    // let primary_circuit = CircomCircuit {
+    //     r1cs: r1cs_circom,
+    //     witness: None,
+    // };
+    // let secondary_circuit = TrivialTestCircuit::<<G2 as Group>::Scalar>::default();
+
+    // let proving_time = start_timer!(|| "Proving time");
+    // let res = nova_snark::parallel_prover::par_digest_folds(
+    //     pp_par,
+    //     folds,
+    //     primary_circuit,
+    //     secondary_circuit,
+    // );
+    // assert!(res.is_ok());
+    // end_timer!(proving_time);
+    //--------------------------------
+
+    // NOTE: Verify not implemented in parallel case yet
 
     let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
 
@@ -112,6 +155,176 @@ fn bench(iteration_count: usize, per_iteration_count: usize) -> (Duration, Durat
     );
     let verifier_time = start.elapsed();
     assert!(res.is_ok());
+
+    //produce a compressed SNARK
+    // println!("Generating a CompressedSNARK using Spartan with IPA-PC...");
+    // let start = Instant::now();
+    // type S1 = nova_snark::spartan_with_ipa_pc::RelaxedR1CSSNARK<G1>;
+    // type S2 = nova_snark::spartan_with_ipa_pc::RelaxedR1CSSNARK<G2>;
+    // let res = CompressedSNARK::<_, _, _, _, S1, S2>::prove(&pp, &recursive_snark);
+    // println!(
+    //     "CompressedSNARK::prove: {:?}, took {:?}",
+    //     res.is_ok(),
+    //     start.elapsed()
+    // );
+    // assert!(res.is_ok());
+    // let compressed_snark = res.unwrap();
+
+    // // verify the compressed SNARK
+    // println!("Verifying a CompressedSNARK...");
+    // let start = Instant::now();
+    // let res = compressed_snark.verify(
+    //     &pp,
+    //     iteration_count,
+    //     start_public_input.clone(),
+    //     z0_secondary,
+    // );
+    // println!(
+    //     "CompressedSNARK::verify: {:?}, took {:?}",
+    //     res.is_ok(),
+    //     start.elapsed()
+    // );
+    // assert!(res.is_ok());
+
+    let verifier_time = Duration::from_secs(999);
+    (prover_time, verifier_time)
+}
+
+fn bench_par(iteration_count: usize, per_iteration_count: usize) -> (Duration, Duration) {
+    let root = current_dir().unwrap();
+
+    let depth = iteration_count;
+
+    let circuit_file = root.join("examples/bitcoin/circom/bitcoin_benchmark.r1cs");
+    let r1cs_circom = load_r1cs(&FileLocation::PathBuf(circuit_file));
+    let witness_generator_file =
+        root.join("examples/bitcoin/circom/bitcoin_benchmark_cpp/bitcoin_benchmark");
+
+    // load serde json
+    let btc_blocks: Blocks =
+        serde_json::from_str(include_str!("bitcoin/fetcher/btc-blocks.json")).unwrap();
+
+    let start_public_input = vec![
+        F1::from_str_vartime(&btc_blocks.prevBlockHash[0]).unwrap(),
+        F1::from_str_vartime(&btc_blocks.prevBlockHash[1]).unwrap(),
+    ];
+
+    let mut private_inputs = Vec::new();
+
+    for i in 0..iteration_count {
+        let mut private_input = HashMap::new();
+        private_input.insert(
+            "blockHashes".to_string(),
+            json!(
+                btc_blocks.blockHashes
+                    [i * per_iteration_count..i * per_iteration_count + per_iteration_count]
+            ),
+        );
+        private_input.insert(
+            "blockHeaders".to_string(),
+            json!(
+                btc_blocks.blockHeaders
+                    [i * per_iteration_count..i * per_iteration_count + per_iteration_count]
+            ),
+        );
+        private_inputs.push(private_input);
+    }
+
+    // println!("{:?} {:?}", start_public_input, private_inputs);
+
+    let pp = create_public_params(r1cs_circom.clone());
+
+    let pp_par: nova_snark::parallel_prover::PublicParams<
+    G1,
+    G2,
+    CircomCircuit<F1>,
+    TrivialTestCircuit<F2>,
+> = create_public_params_par(r1cs_circom.clone());
+
+    println!(
+        "Number of constraints per step (primary circuit): {}",
+        pp.num_constraints().0
+    );
+    println!(
+        "Number of constraints per step (secondary circuit): {}",
+        pp.num_constraints().1
+    );
+
+    println!(
+        "Number of variables per step (primary circuit): {}",
+        pp.num_variables().0
+    );
+    println!(
+        "Number of variables per step (secondary circuit): {}",
+        pp.num_variables().1
+    );
+
+    // Sequential case
+    // println!("Creating a RecursiveSNARK...");
+    // let start = Instant::now();
+    // let recursive_snark = create_recursive_circuit(
+    //     FileLocation::PathBuf(witness_generator_file),
+    //     r1cs_circom,
+    //     private_inputs,
+    //     start_public_input.clone(),
+    //     &pp,
+    // )
+    // .unwrap();
+    // let prover_time = start.elapsed();
+    // println!("RecursiveSNARK creation took {:?}", start.elapsed());
+    //--------------------------------
+
+    //Parallel case
+    println!("Creating a RecursiveSNARK parallel...");
+    let start = Instant::now();
+    let folds: Vec<FoldInput<G1>> = nova_scotia::prepare_folds(
+        FileLocation::PathBuf(witness_generator_file),
+        r1cs_circom.clone(),
+        private_inputs,
+        depth,
+        // This is wrong and we should be passing all the PIs here.
+        start_public_input.clone(),
+    );
+
+    let primary_circuit = CircomCircuit {
+        r1cs: r1cs_circom,
+        witness: None,
+    };
+    let secondary_circuit = TrivialTestCircuit::<<G2 as Group>::Scalar>::default();
+
+    let proving_time = start_timer!(|| "Proving time");
+    let res = nova_snark::parallel_prover::par_digest_folds(
+        pp_par,
+        folds,
+        primary_circuit,
+        secondary_circuit,
+    );
+    assert!(res.is_ok());
+    //end_timer!(proving_time);
+    let prover_time = start.elapsed();
+
+    //--------------------------------
+
+    // NOTE: Verify not implemented in parallel case yet
+
+    // let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
+
+    // // verify the recursive SNARK
+    // println!("Verifying a RecursiveSNARK...");
+    // let start = Instant::now();
+    // let res = recursive_snark.verify(
+    //     &pp,
+    //     iteration_count,
+    //     start_public_input.clone(),
+    //     z0_secondary.clone(),
+    // );
+    // println!(
+    //     "RecursiveSNARK::verify: {:?}, took {:?}",
+    //     res,
+    //     start.elapsed()
+    // );
+    // let verifier_time = start.elapsed();
+    // assert!(res.is_ok());
 
     // produce a compressed SNARK
     // println!("Generating a CompressedSNARK using Spartan with IPA-PC...");
@@ -142,12 +355,15 @@ fn bench(iteration_count: usize, per_iteration_count: usize) -> (Duration, Durat
     //     start.elapsed()
     // );
     // assert!(res.is_ok());
+    //(prover_time, verifier_time)
+    let verifier_time = Duration::from_secs(999);
     (prover_time, verifier_time)
 }
 
 fn main() {
     // create benchmark file
     let mut file = std::fs::File::create("examples/bitcoin/benchmark.csv").unwrap();
+    
     file.write_all(b"iteration_count,per_iteration_count,prover_time,verifier_time\n")
         .unwrap();
     for i in 1..=5 {
@@ -160,7 +376,10 @@ fn main() {
             .output()
             .expect("failed to execute process");
 
-        let (prover_time, verifier_time) = bench(j, i);
+
+        let (prover_time, verifier_time) = bench_seq(j, i);
+        let (prover_time, verifier_time) = bench_par(j, i);
+
         file.write_all(format!("{},{},{:?},{:?}\n", j, i, prover_time, verifier_time).as_bytes())
             .unwrap();
     }
--- nova/examples/bitcoin/circom/compile.sh
+++ nova/examples/bitcoin/circom/compile.sh
@@ -1,5 +1,6 @@
 cd examples/bitcoin/circom
-ghead -n -1 bitcoin.circom > bitcoin_benchmark.circom
+# ghead on OSX, head on Linux
+head -n -1 bitcoin.circom > bitcoin_benchmark.circom
 echo "component main { public [step_in] } = Main($1);" >> bitcoin_benchmark.circom
 circom bitcoin_benchmark.circom --r1cs --sym --c --prime vesta
 cd bitcoin_benchmark_cpp && make
